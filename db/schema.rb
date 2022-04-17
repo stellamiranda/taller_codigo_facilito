fe36c0638f825785e4c605f8d503f7f61a07cd7e
Stella Miranda <stellamariam@gmail.com>
2022-04-16 22:15:16 -0700

    Add style to signup

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_044414) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_044414) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
