63b333bc8e97351f5b7922556c78182e419793cd
Stella Miranda <stellamariam@gmail.com>
2022-04-19 18:13:28 -0700

    Style and identation fixes

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -60,13 +60,13 @@ class TweeetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tweeet
-      @tweeet = Tweeet.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_tweeet
+    @tweeet = Tweeet.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def tweeet_params
-      params.require(:tweeet).permit(:tweeet)
-    end
+  # Only allow a list of trusted parameters through.
+  def tweeet_params
+    params.require(:tweeet).permit(:tweeet)
+  end
 end
